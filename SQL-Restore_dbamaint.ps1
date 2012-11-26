fa07e1437a7187dfdded214e32e2d42bd77c6d90
Michael J. Messano <mmessano@dexma.com>
2012-11-26 12:57:53 -0600

    Fixed restore statement, added run-query function, added owner change to sa for restored database.
    Signed-off-by: Michael J. Messano <mmessano@dexma.com>

--- SQL-Restore_dbamaint.ps1
+++ SQL-Restore_dbamaint.ps1
@@ -1,9 +1,55 @@
 ﻿# SQL-Restore_dbamaint.ps1
 
 param( 
-	$SQLServer = 'STGSQLDOC710',
-	$BAKFile = 'e:\Dexma\MSSQL\Bak\',
+	$SQLServer,
+	$BAKFile = '\\xsqlutil18\e$\MSSQL10.MSSQLSERVER\MSSQL\BAK\dbamaint\dbamaint_backup_2012_11_25_230001_7339465.bak',
 	$FilePrefix = 'Log',
 	[switch]$Log
 )
+
+$ScriptName = [system.io.path]::GetFilenameWithoutExtension($MyInvocation.InvocationName)
+
+IF (!$SQLServer )
+{
+	$SQLServer = Get-Content "E:\Dexma\data\ServerList.txt"
+}
+
+# Functions
+function Run-Query()
+{
+	param (
+		$SqlQuery
+		, $SqlServer
+		, $SqlCatalog
+		, $SqlUser
+		, $SqlPass
+	)
+	
+	$SqlConnection = New-Object System.Data.SqlClient.SqlConnection("Data Source=$SqlServer;Integrated Security=SSPI;Initial Catalog=$SqlCatalog;");
+	
+	$SqlCmd = New-Object System.Data.SqlClient.SqlCommand
+	$SqlCmd.CommandText = $SqlQuery
+	$SqlCmd.Connection = $SqlConnection
+	
+	$SqlAdapter = New-Object System.Data.SqlClient.SqlDataAdapter
+	$SqlAdapter.SelectCommand = $SqlCmd
+	
+	$DataSet = New-Object System.Data.DataSet
+	$a = $SqlAdapter.Fill($DataSet)
+	
+	$SqlConnection.Close()
+	
+	$DataSet.Tables | Select-Object -ExpandProperty Rows
+}
+
+$OwnerUpdate = "EXEC sp_changedbowner 'sa';"
+
+foreach ( $Server IN $SQLServer )
+	{
+	
+	Restore-SQLdatabase -SQLServer $Server -SQLDatabase dbamaint -Path $BAKFile -TrustedConnection
+	
+	Run-Query -SqlQuery $OwnerUpdate -SqlServer $Server -SqlCatalog dbamaint
+	
+	}
 
